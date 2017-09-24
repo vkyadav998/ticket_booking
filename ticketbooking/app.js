53d90e70e46fbea06ab8daceca9e58780b151cb5
Vivek - Vipin <vivekk252@gmail.com>
2017-09-24 18:51:28 +0530

    bower compnent  for client side libreary

--- ticketbooking/app.js
+++ ticketbooking/app.js
@@ -17,10 +17,6 @@ let reqservice = require('./server/manager/rout');
  
 app.use('/', reqservice);
 
-app.get('/', function(req, res) {
-    res.json({ message: 'Application is running here. hit on [ /api ] to connect database.' });
-});
-
 app.listen("3000",function(){
     console.log("!! server started on port 3000 !!");
 });
