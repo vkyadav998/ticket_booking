72fc2f053a922d836da0e049c6d89022bc0e8342
Vivek - Vipin <vivekk252@gmail.com>
2017-10-01 00:44:35 +0530

    http intigration

--- ticketbooking/src/app/app.component.ts
+++ ticketbooking/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<div class="text-center">
+                <h1 *ngFor="let ticket of tickets">{{ticket.source}}</h1>
+              </div>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private _http: Http){  };
+
+  tickets : any [];
+
   title = 'app';
+  ngOnInit() : void {
+    this._http.get('booking_service/getall_ticket').subscribe(data => {
+      this.tickets = data.json().allItom;
+    });
+    // this.tickets = [
+    //   {"source":"vipin"},
+    //   {"source":"ganesh"},
+    //   {"source":"vijay"},
+    //   {"source":"sachin"}
+    // ]
+  }
 }
